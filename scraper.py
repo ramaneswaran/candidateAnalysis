6a3d18ea9de6201a1dbe7474aadfc792ef21cabb
ramaneswaran <ramannewdelhi44@gmail.com>
2019-09-22 01:47:10 +0530

    Implemented the API in a class

--- scraper.py
+++ scraper.py
@@ -1,17 +1,42 @@
-from dotenv import load_dotenv
-load_dotenv()
+#  Importing the required packages
 
 import os
 import tweepy
+from dotenv import load_dotenv
+load_dotenv()
+
+class scraper():
+    def __init__(self, CONSUMER_TOKEN, CONSUMER_SECRET):
+        self.CONSUMER_SECRET  = CONSUMER_SECRET
+        self.CONSUMER_TOKEN = CONSUMER_TOKEN
+
+    def auth(self):
+        #OAuth 2 Authentication
+
+        self.auth =  tweepy.AppAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+        self.api = tweepy.API(self.auth)
+
+    def display(self):
+        for tweet in tweepy.Cursor(self.api.search, q='@BernieSanders@SenSanders').items(10):
+            print(tweet.text)
+
+
+
 
 CONSUMER_TOKEN = os.getenv("CONSUMER_TOKEN")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 
-#OAuth 2 Authentication
 
-auth =  tweepy.AppAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+scrape = scraper(CONSUMER_TOKEN,CONSUMER_SECRET)
+scrape.auth()
+scrape.display()
 
-api = tweepy.API(auth)
 
-for tweet in tweepy.Cursor(api.search, q='#trump2020').items(1):
-    print(tweet.text)
+#Creating list of key words
+
+sanders = ['#Sanders2020', '@SenSanders', '@BernieSanders']
+
+trump = ['#Trump2020', '@realDonaldTrump']
+
+biden = ['#Biden2020', '@JoeBiden']
+
