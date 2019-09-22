6f514558349c84353d8ebe8b98348d06c302812b
ramaneswaran <ramannewdelhi44@gmail.com>
2019-09-22 16:13:42 +0530

    Added flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+import analyse 
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', candidates = analyse.candidates)
+
+if __name__ == '__main__':
+    app.run(debug=True)
